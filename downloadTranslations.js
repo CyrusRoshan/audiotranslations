97a32e1fabc6bbc02c351f933b13dec11b011735
Cyrus Roshan <CyrusRoshan@users.noreply.github.com>
2016-12-25 22:24:30 -0600

    Actually kind of clean...

--- downloadTranslations.js
+++ downloadTranslations.js
@@ -1,15 +1,19 @@
 document.querySelector('button').onclick = () => {
-        var fromLang = document.querySelector('#fromLang').value;
         var toLang = document.querySelector('#toLang').value;
 
         var words = document.querySelector('#words').value.split(',');
 
-        var downloadFrom = document.querySelector('#settings [name="downloadFrom"]').checked;
-        var downloadTo = document.querySelector('#settings [name="downloadTo"]').checked;
+        words.forEach((word, i) => {
+                word = word.trim();
+                setTimeout(() => {
+                        chrome.downloads.download({
+                                url: `https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=${word}&tl=${toLang}&total=1&idx=0&textlen=${word.length}`,
+                                filename: `${word}.mp3`,
+                        }, ()=>{})
+                }, getRandomInt(0, 2000) * i);
+        })
+}
 
-        chrome.storage.sync.get('url', function(url) {
-        	if (!url) {
-                        
-                }
-        });
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
